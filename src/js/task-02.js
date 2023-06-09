202300a59f308cb6f0c2149d7f9fe9528830370a
Наталья ЧеркашинаКоцько <cherkashyna.n@gmail.com>
2023-06-09 15:28:25 +0300

    Finished task 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,11 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector('#ingredients');
+const markup = ingredients
+  .map((ingredient) => `<li class = "item">${ingredient}</li>`)
+  .join("");
+
+list.innerHTML = markup;
+
