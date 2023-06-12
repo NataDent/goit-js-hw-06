63f1fab89c8874a1238f917918b8b98f9b79374b
Наталья ЧеркашинаКоцько <cherkashyna.n@gmail.com>
2023-06-12 14:29:36 +0300

    finished task 4

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,18 +1,19 @@
-for (let i = 0; i < 5; i++) {
-  console.log('Hello world!');
-}
-for (let i = 0; i < 5; i++) {
-  console.log('Hello world!');
-}
+const counterValue = {
+  value: 0,
+  increment() { this.value += 1; },
+  decrement() { this.value -= 1; },
+};
 
 const decBtn = document.querySelector('[data-action="decrement"]');
 const incBtn = document.querySelector('[data-action="increment"]');
-let counterValue = document.querySelector('#value');
-counterValue = 0;
-const addCount = () => counterValue += 1;
-const removeCount = () => counterValue -= 1;
-decBtn.addEventListener('click', removeCount);
-incBtn.addEventListener('click', addCount);
-console.log(decBtn);
-console.log(removeCount);
-incBtn.addEventListener("click", () => {
+const valuEl = document.querySelector('#value');
+
+decBtn.addEventListener('click', function () {
+  counterValue.decrement();
+  valuEl.textContent = counterValue.value; 
+});
+incBtn.addEventListener('click', function () {
+  counterValue.increment();
+  valuEl.textContent = counterValue.value;
+});
+
