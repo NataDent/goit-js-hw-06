940235b1e9c5115390ee5903c7ab922eda23a691
Наталья ЧеркашинаКоцько <cherkashyna.n@gmail.com>
2023-06-14 00:26:46 +0300

    try 2

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -3,20 +3,32 @@ console.dir(inputEl);
 const dataLength = inputEl.getAttribute('data-length');
 const inputValueLength = inputEl.value.length;
 const checkInputValueLength = () => {
-    if (Number(dataLength) === Number(inputValueLength)) {
-        inputEl.classList.add('valid');
-       
-    }
-    else
-    if (inputEl.classList.contains('invalid')) {
+    if ( Number(inputValueLength) === Number(dataLength)) {
+      if (!inputEl.classList.contains('invalid')) {
+        inputEl.classList.add('valid')
+      } else {
         inputEl.classList.replace('invalid', 'valid')
-        
-    } 
-    else {
-        inputEl.classList.add('invalid');  
+      }  
+    } else {
+      inputEl.classList.add('invalid')  
     }
-   
 }
+   
+// {
+//     if (Number(dataLength) === Number(inputValueLength) && !inputEl.classList.contains('invalid'))) {
+//         inputEl.classList.add('valid');
+       
+//     }
+//     else {
+//     if (Number(dataLength) === Number(inputValueLength) && inputEl.classList.contains('invalid'))) {
+//     inputEl.classList.replace('invalid', 'valid');
+//       }  
+//     } 
+//     else {
+//         inputEl.classList.add('invalid');  
+//     }
+  
+// }
 
 inputEl.addEventListener('blur', checkInputValueLength);
 
