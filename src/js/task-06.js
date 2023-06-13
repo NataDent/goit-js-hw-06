4c30d5d50c8a54e9aa7201eef70045c24c3e7719
Наталья ЧеркашинаКоцько <cherkashyna.n@gmail.com>
2023-06-13 23:32:24 +0300

    Task 6 dasn't work

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -3,13 +3,19 @@ console.dir(inputEl);
 const dataLength = inputEl.getAttribute('data-length');
 const inputValueLength = inputEl.value.length;
 const checkInputValueLength = () => {
-    if (Number(dataLength) === Number(inputValueLength && !inputEl.classList.contains('.invalid'))) {
-        inputEl.classList.replace('invalid', 'valid');
-        
+    if (Number(dataLength) === Number(inputValueLength)) {
+        inputEl.classList.add('valid');
+       
     }
     else
-    
-    {inputEl.classList.add('invalid')}
+    if (inputEl.classList.contains('invalid')) {
+        inputEl.classList.replace('invalid', 'valid')
+        
+    } 
+    else {
+        inputEl.classList.add('invalid');  
+    }
+   
 }
 
 inputEl.addEventListener('blur', checkInputValueLength);
