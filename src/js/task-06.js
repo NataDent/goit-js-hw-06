283992cc1dc659734069e1fe76bb5d8e0ac9b2d8
Наталья ЧеркашинаКоцько <cherkashyna.n@gmail.com>
2023-06-14 13:04:45 +0300

    Зміна коду

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,37 +1,14 @@
-const inputEl = document.querySelector('#validation-input');
-console.dir(inputEl);
-const dataLength = inputEl.getAttribute('data-length');
-const inputValueLength = inputEl.value.length;
-const checkInputValueLength = () => {
-    if ( Number(inputValueLength) === Number(dataLength)) {
-      if (!inputEl.classList.contains('invalid')) {
-        inputEl.classList.add('valid')
-      } else {
-        inputEl.classList.replace('invalid', 'valid')
-      }  
-    } else {
-      inputEl.classList.add('invalid')  
+const inputEl = document.querySelector('#validation-input')
+console.log(inputEl);
+const dataLengthEl = inputEl.getAttribute('data-length')
+console.log(dataLengthEl);
+const checkInputLength = () => {
+    if (Number(dataLengthEl) === Number(inputEl.value.length) & !inputEl.classList.contains('invalid'))
+    { inputEl.classList.add('valid') }
+    else if (Number(dataLengthEl) === Number(inputEl.value.length) & inputEl.classList.contains('invalid')) {
+        inputEl.classList.replace('invalid','valid')
+    }
+    else {
+    inputEl.classList.add('invalid')
     }
 }
-   
-// {
-//     if (Number(dataLength) === Number(inputValueLength) && !inputEl.classList.contains('invalid'))) {
-//         inputEl.classList.add('valid');
-       
-//     }
-//     else {
-//     if (Number(dataLength) === Number(inputValueLength) && inputEl.classList.contains('invalid'))) {
-//     inputEl.classList.replace('invalid', 'valid');
-//       }  
-//     } 
-//     else {
-//         inputEl.classList.add('invalid');  
-//     }
-  
-// }
-
-inputEl.addEventListener('blur', checkInputValueLength);
-
-console.dir(dataLength);
-console.log(inputEl.dataset.length);
-console.log(inputEl.dataset);
