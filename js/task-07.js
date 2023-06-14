56f3fbb887039554d07a674f819fd05dbc2bbdc3
Наталья ЧеркашинаКоцько <cherkashyna.n@gmail.com>
2023-06-15 00:59:05 +0300

    7 task is in progress

---
+++ js/task-07.js
@@ -0,0 +1,14 @@
+const inputEl = document.querySelector('#font-size-control');
+console.dir(inputEl);
+const textEl = document.querySelector('#text');
+console.dir(textEl);
+
+const onInputHandler = (evt) => { 
+    
+    textEl.style.fontSize = evt.currentTarget.value;
+   
+    console.log(textEl.style.fontSize)
+    console.dir(evt.currentTarget.value)
+}
+
+inputEl.addEventListener('input', onInputHandler);
