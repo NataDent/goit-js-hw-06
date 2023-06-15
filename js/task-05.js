1109f72379c7cb47b359092ef4f1f27018670936
Наталья ЧеркашинаКоцько <cherkashyna.n@gmail.com>
2023-06-15 15:01:01 +0300

    Доробила умову таск 5

--- js/task-05.js
+++ js/task-05.js
@@ -2,6 +2,11 @@ const inputEl = document.querySelector('#name-input');
 
 const userName = document.querySelector('#name-output');
 
-inputEl.addEventListener('input', (evt) => userName.textContent = evt.currentTarget.value);
+inputEl.addEventListener('input', (evt) => {
+    if (!evt.currentTarget.value) {
+        userName.textContent = 'Anonimus'
+    } else
+       { userName.textContent = evt.currentTarget.value};
+});
 
 
--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,7 @@
+const formEl = document.querySelector('login-form');
+formEl.addEventListener('submit', onSubmit);
+function onSubmit(evt) {
+    evt.preventDefault();
+    console.log(evt.currentTarget);
 
+}
