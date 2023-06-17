dc166ecc0ccd8b4ed0bcab9059af4257a0aecfb4
Наталья ЧеркашинаКоцько <cherkashyna.n@gmail.com>
2023-06-17 11:00:51 +0300

    9 nfsr is in progress

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,19 @@
+
+const selectors = {
+  spanEl: document.querySelector('.color'),
+  buttonEl: document.querySelector('.change-color'),
+  bodyEl: document.querySelector('body'),
+};
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function onClick() {
+  selectors.bodyEl.style.backgroundColor = ''
+  selectors.spanEl.textContent = getRandomHexColor
+}
+console.log(getRandomHexColor)
+// buttonEl.addEventListener('click', onClick);
