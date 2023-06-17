d8a07aea2d1ebc05cde6d60e508f8e1de16a44f1
Наталья ЧеркашинаКоцько <cherkashyna.n@gmail.com>
2023-06-18 00:55:11 +0300

    Task 10 is in progress

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,21 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const inputEl = document.querySelector('input');
+console.dir(inputEl);
+const btnCreate = document.querySelector('button[data-create]');
+console.log(btnCreate);
+const btnDestroy = document.querySelector('button[data-destroy]');
+console.log(btnDestroy);
+const boxEl = document.querySelector('#boxes');
+console.log(boxEl);
+btnCreate.addEventListener('click', createBoxes);
+
+function createBoxes(amount) {
+  const size = 30;
+ const markup = `<div width=${size}px heigt =${size}px></div>`
+  for (let index = 1; index <= amount; index += 1) {
+    boxEl.insertAdjacentHTML('beforeend', markup)
+  }
+  console.log(markup)
+}
